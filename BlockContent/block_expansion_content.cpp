b2630e913a6409d76ae4d747fc43db3e72db2d26
MrJambix <mrjambix1993@gmail.com>
2024-07-11 03:29:50 +0200

    Update block_expansion_content.cpp
    Re-added comments for each mapID to better track content

--- BlockContent/block_expansion_content.cpp
+++ BlockContent/block_expansion_content.cpp
@@ -17,9 +17,36 @@ void block_expansion_content::OnMapEnter(Player* player, Map* map)
     {
         uint32 mapId = map->GetId();
         std::set<uint32> blockedRaids = {
-            249, 509, 531, 533, // Vanilla
-            532, 544, 548, 550, 552, 553, 554, 555, 556, 557, 558, 564, 565, 568, 580, // TBC
-            533, 603, 615, 616, 624, 631, 649, 724 // WotLK
+            // Vanilla WoW Raids
+            249, // Onyxia's Lair
+            509, // Ruins of Ahn'Qiraj
+            531, // Temple of Ahn'Qiraj
+            533, // Naxxramas
+            // The Burning Crusade Raids
+            532, // Karazhan
+            544, // Magtheridon's Lair
+            548, // Serpentshrine Cavern
+            550, // Tempest Keep
+            552, // The Arcatraz
+            553, // The Botanica
+            554, // The Mechanar
+            555, // Shadow Labyrinth
+            556, // Sethekk Halls
+            557, // Mana-Tombs
+            558, // Auchenai Crypts
+            564, // Black Temple
+            565, // Gruul's Lair
+            568, // Zul'Aman
+            580, // Sunwell Plateau
+            // Wrath of the Lich King Raids
+            533, // Naxxramas (Revisited in WotLK)
+            603, // Ulduar
+            615, // The Obsidian Sanctum
+            616, // The Eye of Eternity
+            624, // Vault of Archavon
+            631, // Icecrown Citadel
+            649, // Trial of the Crusader
+            724, // The Ruby Sanctum
         };
 
         if (blockedRaids.find(mapId) != blockedRaids.end())
@@ -32,14 +59,45 @@ void block_expansion_content::OnMapEnter(Player* player, Map* map)
     {
         uint32 mapId = map->GetId();
         std::set<uint32> blockedDungeons = {
-            269, 540, 542, 543, 546, 547, 545, 548, 550, 553, 554, 556, 557, 558, 559, // TBC
-            574, 575, 576, 578, 595, 599, 600, 601, 602, 604, 608, 619, 632, 650, 658, 668 // WotLK
+            // The Burning Crusade Dungeons
+            269, // Opening of the Dark Portal
+            540, // The Shattered Halls
+            542, // The Blood Furnace
+            543, // Hellfire Ramparts
+            546, // The Underbog
+            547, // The Slave Pens
+            545, // The Steamvault
+            548, // Coilfang Reservoir
+            550, // Tempest Keep: The Arcatraz
+            553, // Tempest Keep: The Botanica
+            554, // Tempest Keep: The Mechanar
+            556, // Auchindoun: Sethekk Halls
+            557, // Auchindoun: Mana-Tombs
+            558, // Auchindoun: Auchenai Crypts
+            559, // Auchindoun: Shadow Labyrinth
+            // Wrath of the Lich King Dungeons
+            574, // Utgarde Keep
+            575, // Utgarde Pinnacle
+            576, // The Nexus
+            578, // The Oculus
+            595, // The Culling of Stratholme
+            599, // Halls of Stone
+            600, // Drak'Tharon Keep
+            601, // The Violet Hold
+            602, // Halls of Lightning
+            604, // Gundrak
+            608, // The Violet Hold
+            619, // Ahn'kahet: The Old Kingdom
+            632, // The Forge of Souls
+            650, // Trial of the Champion
+            658, // Pit of Saron
+            668, // Halls of Reflection
         };
 
         if (blockedDungeons.find(mapId) != blockedDungeons.end())
         {
             player->GetSession()->SendNotification("You are not allowed to enter this dungeon.");
-            player->TeleportTo(player->m_homebindMapId, player's m_homebindX, player's m_homebindY, player's m_homebindZ, player's GetOrientation());
+            player->TeleportTo(player->m_homebindMapId, player->m_homebindX, player's m_homebindY, player's m_homebindZ, player's GetOrientation());
         }
     }
 }
